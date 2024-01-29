a549895ba6f8b3978231d719f9eece448345bbf4
EricaMarzi <erica.marzi.1993@gmail.com>
2024-01-29 17:03:16 +0100

    lettura dati

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ In un'applicazione Vue, generare 10 indirizzi email e stamparli in pagina all'in
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati. (per testare il funzionamento di questo bonus, provate a rallentare la connessione col tab network, mettendo lo slow 3g)
  */
+
+const {createApp} = Vue;
+
+//& Boolean API
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+const app = createApp ({
+    data: () => ({}),
+    created() {
+        axios.get(endpoint).then(res => {
+            //* Da eseguire in caso di successo
+            console.log(res.data)
+        })
+        .catch((err) => {
+            //! Da eseguire in caso di errore (es un alert)
+        })
+        .then(() => {
+            //^ Da eseguire in ogni caso (es un loading)
+        })
+    }
+});
+
+app.mount('#root');
